6c9b91efe2bb7569ec2f308c2e2f5f9028086f1f
Ali Johnson <ali.johnson@Nology.local>
2019-08-10 15:48:07 +0100

    built non functioning searchBar

--- src/Container/ToDoContainer.jsx
+++ src/Container/ToDoContainer.jsx
@@ -3,20 +3,59 @@ import React, { Component } from "react";
 import ToDoSingle from "../components/ToDoitem";
 
 class ToDoContainer extends Component {
-  state = { inputValue: "", toDoArray: [] };
+  state = {
+    inputValue: "",
+    editValue: "",
+    searchValue: "",
+    toDoArray: [],
+    filteredToDoArray: []
+  };
 
-  handleChange = event => {
+  handleChangeNewItem = event => {
     this.setState({ inputValue: event.target.value });
+    //  console.log(event.target.value);
+  };
+
+  handleChangeSearch = event => {
+    this.setState({ searchValue: event.target.value });
+    console.log(this.state.searchValue);
   };
 
   submitToDo = event => {
     event.preventDefault();
-
-    console.log("barry");
     this.setState({
       toDoArray: [...this.state.toDoArray, this.state.inputValue],
       inputValue: ""
     });
+  };
+
+  submitSearch = event => {
+    event.preventDefault();
+
+    console.log(this.state.inputValue);
+  };
+
+  handleEditChange = event => {
+    // event.preventDefault();
+    this.setState({ editValue: event.target.value });
+    console.log(this.state.editValue);
+  };
+
+  //can I edit an item in an array
+  handleEditSubmit = itemToEdit => {
+    let editArr = this.state.toDoArray.map(item => {
+      if (item === itemToEdit) {
+        return this.state.editValue;
+      } else {
+        return item;
+      }
+    });
+    this.setState({ toDoArray: editArr });
+  };
+
+  handleDelete = itemToBeDeleted => {
+    let newArr = this.state.toDoArray.filter(item => item !== itemToBeDeleted);
+    this.setState({ toDoArray: newArr });
   };
 
   render() {
@@ -24,19 +63,31 @@ class ToDoContainer extends Component {
       <section>
         <h1>To-Do List</h1>
         <form>
-          <label>
-            To Do
-            <input
-              type="text"
-              value={this.state.inputValue}
-              onChange={this.handleChange}
-            />
-          </label>
+          <label>Add Item </label>
+          <input
+            type="text"
+            value={this.state.inputValue}
+            onChange={this.handleChangeNewItem}
+          />
           <input type="submit" value="Submit" onClick={this.submitToDo} />
+
+          <label>Search</label>
+          <input
+            type="text"
+            //   value={this.state.inputValue}
+            onChange={this.handleChangeSearch}
+          />
+          <input type="submit" value="Search" onClick={this.submitSearch} />
         </form>
 
-        {this.state.toDoArray.map((toDoSingle, index) => (
-          <ToDoSingle toDoText={toDoSingle} key={index} />
+        {this.state.toDoArray.map((item, index) => (
+          <ToDoSingle
+            item={item}
+            key={index}
+            handleDelete={this.handleDelete}
+            handleEditSubmit={this.handleEditSubmit}
+            handleEditChange={this.handleEditChange}
+          />
         ))}
       </section>
     );
--- src/components/ToDoitem.jsx
+++ src/components/ToDoitem.jsx
@@ -2,24 +2,39 @@ import React, { Component } from "react";
 import styles from "./ToDoItem.module.scss";
 
 class ToDoSingle extends Component {
-  state = { isDone: false, isDeleted: false };
+  state = { isDone: false, isEdit: false };
 
   markAsDone = () => {
     this.setState({ isDone: !this.state.isDone });
   };
 
-  deleteItem = () => {
-    this.setState({ isDeleted: !this.state.isDeleted });
+  markToEdit = () => {
+    this.setState({ isEdit: !this.state.isDone });
   };
+
+  //   deleteItem = () => {
+  //     this.setState({ isDeleted: !this.state.isDeleted });
+  //   };
 
   render() {
     const clickedDone = this.state.isDone ? styles.markedAsDone : null;
+    const clickedEdit = this.state.isEdit ? null : styles.editForm;
     return (
       <section>
-        <h2 className={clickedDone}>{this.props.toDoText}</h2>
+        <h2 className={clickedDone}>{this.props.item}</h2>
+        <form className={clickedEdit}>
+          <input type="text" onChange={this.props.handleEditChange} />
+          <input
+            type="submit"
+            value="Submit"
+            onClick={() => this.props.handleEditSubmit(this.props.item)}
+          />
+        </form>
         <button onClick={this.markAsDone}>Mark As Done</button>
-        <button>Delete</button>
-        <button>Edit</button>{" "}
+        <button onClick={() => this.props.handleDelete(this.props.item)}>
+          Delete
+        </button>
+        <button onClick={this.markToEdit}>Edit</button>{" "}
       </section>
     );
   }
