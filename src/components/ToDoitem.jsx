181cc225135997b6278b0afa09b59b28df321e26
Ali Johnson <ali.johnson@Nology.local>
2019-08-10 10:13:15 +0100

    marked card function working

--- src/components/ToDoitem.jsx
+++ src/components/ToDoitem.jsx
@@ -1,21 +1,25 @@
 import React, { Component } from "react";
+import styles from "./ToDoItem.module.scss";
 
 class ToDoSingle extends Component {
-  state = { isDone: false };
+  state = { isDone: false, isDeleted: false };
 
   markAsDone = () => {
-    this.setState({ isDone: true });
+    this.setState({ isDone: !this.state.isDone });
+  };
+
+  deleteItem = () => {
+    this.setState({ isDeleted: !this.state.isDeleted });
   };
 
   render() {
+    const clickedDone = this.state.isDone ? styles.markedAsDone : null;
     return (
       <section>
-        <h2>
-          {this.props.toDoText}
-          <button onClick={this.markAsDone}>Mark As Done</button>
-          <button>Delete</button>
-          <button>Edit</button>{" "}
-        </h2>
+        <h2 className={clickedDone}>{this.props.toDoText}</h2>
+        <button onClick={this.markAsDone}>Mark As Done</button>
+        <button>Delete</button>
+        <button>Edit</button>{" "}
       </section>
     );
   }
